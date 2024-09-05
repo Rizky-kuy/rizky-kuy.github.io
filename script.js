8df73d6dca471f03b9f78c819bf3eb3838ef2641
Rizky-kuy <rizkyarsaputra06@gmail.com>
2024-09-05 17:07:37 +0700

    Update script.js

--- script.js
+++ script.js
@@ -36,14 +36,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Gamers","Student"],
+        strings: ["Frond-end","Student"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Gamers","Student"],
+        strings: ["Frond-end","Student"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
